9106cb578644868dc2c0ff11e16bdb3709836517
zhouyuan <3294207721@qq.com>
2021-05-27 21:05:06 +0800

    first

---
+++ Python_Basics/4.dime.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+dem = (200,90)
+print(dem)
+print(dem[0])
+
+#dem[1] = 300#eror
+
+
+dim = (10, 40,    29,  90,  30)
+for d in dim : 
+    print(d)
+
+dem = (323, 4345)
+print(dem)
---
+++ Python_Basics/a.cpp
@@ -0,0 +1,20 @@
+/*************************************************************************
+	> File Name: a.cpp
+	> Author: zhouyuan
+	> Mail: 3294207721@qq.com 
+	> Created Time: 四  5/27 20:54:34 2021
+ ************************************************************************/
+
+#include<iostream>
+#include<cstring>
+#include<algorithm>
+#include<cstdio>
+#include<queue>
+#include<vector>
+using namespace std;
+ 
+int main() {
+ 
+ 
+    return 0;
+}
